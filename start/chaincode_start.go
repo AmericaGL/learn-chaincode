95baeb6f0ed24665d73aed1b5656db0a8ce678ef
AmericaGL <info@cybercodetwins.com>
2017-01-15 12:15:51 -0800

    write read function to complement putstate and getstate

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -100,3 +100,25 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
+
+//Read function is using the complement to PutState called GetState. 
+//While PutState allows you to set a key/value pair, 
+//GetState lets you read the value for a previously written key
+
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+    var key, jsonResp string
+    var err error
+
+    if len(args) != 1 {
+        return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
+    }
+
+    key = args[0]
+    valAsbytes, err := stub.GetState(key)
+    if err != nil {
+        jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+        return nil, errors.New(jsonResp)
+    }
+
+    return valAsbytes, nil
+}
